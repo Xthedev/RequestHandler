ac2e717ce0515941800c0cfbbe7709428f360036
Xthedev <austinchibuike1@gmail.com>
2021-04-03 23:37:56 +0100

    RequestHandler

---
+++ index.js
@@ -0,0 +1,28 @@
+//
+// ──────────────────────────────────────────────────────────────────────────────────────── I ──────────
+//   :::::: R E S P O N S E   S T O R E D   I N   S T A T E : :  :   :    :     :        :          :
+// ──────────────────────────────────────────────────────────────────────────────────────────────────
+//
+
+
+
+ 
+
+
+//
+// ──────────────────────────────────────────────────────────────────────────────────── I ──────────
+//   :::::: R E Q U E S T   H A N D L E R   M O D U L E : :  :   :    :     :        :          :
+// ──────────────────────────────────────────────────────────────────────────────────────────────
+//
+
+
+const _0x5bfb=['162884yiviGs','1lccvXn','open','482938SLLdzl','513631DqQUNR','7573YowNSr','231662SUcKTZ','106LDfsfi','8tXXwtv','386xROvtn','responseText','send','1508830PrkIWF','onreadystatechange','101epwTGK'];const _0x295b=function(_0x16773a,_0x53d92e){_0x16773a=_0x16773a-0xda;let _0x5bfba5=_0x5bfb[_0x16773a];return _0x5bfba5;};(function(_0x3ca40d,_0x448bf8){const _0x34d096=_0x295b;while(!![]){try{const _0x30579a=-parseInt(_0x34d096(0xdb))+parseInt(_0x34d096(0xde))*parseInt(_0x34d096(0xe3))+parseInt(_0x34d096(0xe1))+-parseInt(_0x34d096(0xe8))+parseInt(_0x34d096(0xdd))*parseInt(_0x34d096(0xe4))+parseInt(_0x34d096(0xe5))*-parseInt(_0x34d096(0xe7))+-parseInt(_0x34d096(0xdc))*parseInt(_0x34d096(0xda));if(_0x30579a===_0x448bf8)break;else _0x3ca40d['push'](_0x3ca40d['shift']());}catch(_0x1ecf60){_0x3ca40d['push'](_0x3ca40d['shift']());}}}(_0x5bfb,0xc82cf));export const RequestHandler=(_0x41ebb1,_0x141225,_0x43ff93,_0x8a64)=>{const _0xf1aa47=_0x295b,_0x2c4a17=_0x41ebb1,_0x24e6e7=new XMLHttpRequest();_0x24e6e7[_0xf1aa47(0xe6)](_0x43ff93,_0x2c4a17),_0x24e6e7[_0xf1aa47(0xe0)](_0x141225),_0x24e6e7[_0xf1aa47(0xe2)]=_0x5730b1=>{const _0x2695dc=_0xf1aa47;if(_0x8a64)_0x8a64(_0x24e6e7[_0x2695dc(0xdf)]);};};
+    
+    
+    
+    
+    //
+    // ──────────────────────────────────────────────────────────────────────────────────── I ──────────
+    //   :::::: R E Q U E S T   H A N D L E R   M O D U L E : :  :   :    :     :        :          :
+    // ──────────────────────────────────────────────────────────────────────────────────────────────
+    //
