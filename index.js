dfd0bfb9ef5b40bda8a8c5b24f12fcba2c1cf0a6
Xthedev <austinchibuike1@gmail.com>
2021-04-04 00:57:48 +0100

    sendingjsondata

--- index.js
+++ index.js
@@ -15,7 +15,51 @@
 // ──────────────────────────────────────────────────────────────────────────────────────────────
 //
 
-const _0x21fe=['onreadystatechange','open','12313KORfBK','42226NJWHSg','565152OsIzbN','31smdDbU','responseText','530645tktEWv','send','290861IiBUix','73733vlRZHL','1bximEU','Content-Type','7wYhKJT','7GMNXWg','506641EUwknc','1iocyzk','1oyHWAU','setRequestHeader'];const _0x3097=function(_0x928472,_0x53222c){_0x928472=_0x928472-0x1cd;let _0x21fee6=_0x21fe[_0x928472];return _0x21fee6;};(function(_0x45c578,_0x449ebd){const _0x180583=_0x3097;while(!![]){try{const _0x573cac=-parseInt(_0x180583(0x1dd))*-parseInt(_0x180583(0x1cd))+parseInt(_0x180583(0x1d7))+-parseInt(_0x180583(0x1cf))*-parseInt(_0x180583(0x1d1))+-parseInt(_0x180583(0x1d5))*parseInt(_0x180583(0x1d8))+parseInt(_0x180583(0x1de))*parseInt(_0x180583(0x1dc))+-parseInt(_0x180583(0x1ce))*parseInt(_0x180583(0x1d6))+-parseInt(_0x180583(0x1da))*parseInt(_0x180583(0x1d0));if(_0x573cac===_0x449ebd)break;else _0x45c578['push'](_0x45c578['shift']());}catch(_0x2d7a1e){_0x45c578['push'](_0x45c578['shift']());}}}(_0x21fe,0xa3c87));export const RequestHandler=(_0x2a324a,_0x189b50,_0x186e28,_0x536f98)=>{const _0x4d61e8=_0x3097,_0x38778a=_0x2a324a,_0x968cf2=new XMLHttpRequest();_0x968cf2['open'](_0x186e28,_0x38778a),_0x968cf2[_0x4d61e8(0x1db)](_0x189b50),_0x968cf2['onreadystatechange']=_0x6be549=>{const _0x526675=_0x4d61e8;if(_0x536f98)_0x536f98(_0x968cf2[_0x526675(0x1d9)]);};};export const RequestHandlerJSON=(_0x3f0ed2,_0x5d7f68,_0x34debb,_0x3cc5be)=>{const _0x490c88=_0x3097,_0x3ac31a=_0x3f0ed2,_0x197010=new XMLHttpRequest();_0x197010[_0x490c88(0x1d4)](_0x34debb,_0x3ac31a),xmlhttp[_0x490c88(0x1d2)](_0x490c88(0x1df),'application/json;charset=UTF-8'),_0x197010[_0x490c88(0x1db)](JSON['stringify'](_0x5d7f68)),_0x197010[_0x490c88(0x1d3)]=_0x204b04=>{if(_0x3cc5be)_0x3cc5be(_0x197010['responseText']);};};
+
+
+
+export const RequestHandler=(url,data,type,callback)=>{
+
+
+    // save('insurance',insurance);
+    const URL = url;
+      const xhr = new XMLHttpRequest();
+      xhr.open(type, URL);
+      xhr.send(data);
+      xhr.onreadystatechange = e => {
+       
+     
+        
+       if(callback) callback(xhr.responseText);
+      };
+     
+    
+    
+    }
+    
+    
+    export const RequestHandlerJSON=(url,data,type,callback)=>{
+    
+    
+        // save('insurance',insurance);
+        const URL = url;
+          const xhr = new XMLHttpRequest();
+
+          xhr.open(type, URL);
+          xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+          xhr.send(JSON.stringify(data));
+          xhr.onreadystatechange = e => {
+           
+         
+            
+           if(callback) callback(xhr.responseText);
+          };
+         
+        
+        
+        }
+    
     //
     // ──────────────────────────────────────────────────────────────────────────────────── I ──────────
     //   :::::: R E Q U E S T   H A N D L E R   M O D U L E : :  :   :    :     :        :          :
